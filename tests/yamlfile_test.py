45de96b11ccbf4ebb6c64ac350b46ad70dc25438
Robert (Bob) L. Jones <3848052+djrlj694@users.noreply.github.com>
2022-01-24 22:11:15 -0500

    test(python): Create yamlfile_test.py

---
+++ tests/yamlfile_test.py
@@ -0,0 +1,71 @@
+"""
+yamlfile_test.py - A module for testing the `YAMLFile` class.
+"""
+import os
+import unittest
+from pathlib import Path
+
+from file import YAMLFile
+
+
+# =========================================================================== #
+# METADATA
+# =========================================================================== #
+
+__author__ = 'Robert (Bob) L. Jones'
+__credits__ = ['Robert (Bob) L. Jones']
+
+__created_date__ = 'Jan 24, 2022'
+__modified_date__ = 'Jan 24, 2022'
+
+
+# =========================================================================== #
+# CONSTANTS
+# =========================================================================== #
+
+
+# -- Filesystem -- #
+
+PREFIX = Path(os.getenv('PREFIX', default='.')).resolve()
+
+ETC = PREFIX / 'etc'
+PARENT = ETC / 'settings'
+
+NAME = 'sample_hello.yaml'
+PATH = PARENT / NAME
+
+
+# =========================================================================== #
+# CLASSES
+# =========================================================================== #
+
+
+class TestYAMLFile(unittest.TestCase):
+
+    def test_YAMLFile(self):
+        """
+        Test the `YAMLFile` class.
+        """
+
+        # Create objects to be tested.
+        file = YAMLFile(path=PATH)
+
+        # Test assertions.
+        self.assertEqual(
+            # Actual
+            file.parse()['environments']['production']['target'],
+            # Expected
+            'DBPROD',
+        )
+
+
+# =========================================================================== #
+# MAIN EXECUTION
+# =========================================================================== #
+
+
+# -- Main Execution -- #
+
+# If this module is in the main module, call the main() function.
+if __name__ == '__main__':
+    unittest.main()
