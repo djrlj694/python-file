812b7a846331e1a862843794f00848df2126207a
Robert (Bob) L. Jones <3848052+djrlj694@users.noreply.github.com>
2022-01-26 07:58:06 -0500

    refactor(python): Update text_test.py
    Split method test_TextFile_init() into 3:
    * test_TextFile_1()
    * test_TextFile_1a()
    * test_TextFile_1b()
    Rename method:
    * test_TextFile_list() -> test_list_TextFile()

--- tests/text_test.py
+++ tests/text_test.py
@@ -49,37 +49,68 @@ PATH2 = PARENT2 / NAME2
 
 class TestTextFile(unittest.TestCase):
 
-    def test_TextFile_init(self):
+    def test_TextFile_1(self):
         """
-        Tests creating a `TextFile` object.
+        Tests creating a `TextFile` object from a text file's name and parent
+        directory path.
         """
 
         # Create objects to be tested.
-        file1 = TextFile(NAME1, parent=PARENT1)
-        file1a = TextFile(path=PATH1A)
-        file1b = TextFile(path=PATH1B)
+        file = TextFile(NAME1, parent=PARENT1)
 
         # Test assertions.
         self.assertEqual(
             # Actual
-            file1.path,
+            file.path,
             # Expected
             PATH1A,
         )
         self.assertEqual(
             # Actual
-            file1a.path,
+            file.path,
             # Expected
-            PATH1A,
+            PATH1B,
         )
+
+    def test_TextFile_1a(self):
+        """
+        Tests creating a `TextFile` object from a text file's path.
+        """
+
+        # Create objects to be tested.
+        file = TextFile(path=PATH1A)
+
+        # Test assertions.
         self.assertEqual(
             # Actual
-            file1b.path,
+            file.path,
             # Expected
             PATH1A,
         )
 
-    def test_TextFile_list(self):
+    def test_TextFile_1b(self):
+        """
+        Tests creating a `TextFile` object from a text file's path.
+        """
+
+        # Create objects to be tested.
+        file = TextFile(path=PATH1B)
+
+        # Test assertions.
+        self.assertEqual(
+            # Actual
+            file.path,
+            # Expected
+            PATH1B,
+        )
+        self.assertEqual(
+            # Actual
+            file.path,
+            # Expected
+            PATH1A,
+        )
+
+    def test_list_TextFile(self):
         """
         Tests rendering a `TextFile` object as a list.
         """
