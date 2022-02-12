fbe711488d474ea5171c1e958c1dc34013eacb8b
Robert (Bob) L. Jones <3848052+djrlj694@users.noreply.github.com>
2022-02-12 13:26:05 -0500

    feat(make): Update Python.mk
    Add CONDITIONAL VARIABLES section w/ vars:
    * VENV
    Add SIMPLY EXPANDED VARIABLES section w/ vars:
    * PYTHON
    * VACTIVATE
    * VPYTHON
    * VPIP
    Add phony targets:
    * py-check
    * py-setup
    * py-test
    * py-clean-venv
    Add non-phony targets:
    * $(VACTIVATE)
    * $(VPIP)
    * $(VPYTHON)

--- .make/platforms/Python.mk
+++ .make/platforms/Python.mk
@@ -13,8 +13,35 @@
 # - Robert (Bob) L. Jones
 #
 # CREATED: Jan 15, 2022
-# REVISED: Feb 08, 2022
+# REVISED: Feb 12, 2022
 # =========================================================================== #
+
+
+# =========================================================================== #
+# CONDITIONAL VARIABLES
+# =========================================================================== #
+
+
+# Python virtual environment directory
+VENV ?= .venv
+
+
+# =========================================================================== #
+# SIMPLY EXPANDED VARIABLES
+# =========================================================================== #
+
+
+# PATH version of `python3` command
+PYTHON := $(shell command -v python3 2> /dev/null)
+
+# Python virtual environment version of the `activate` command
+VACTIVATE := $(VENV)/bin/activate
+
+# Python virtual environment version of the `python` command
+VPYTHON := $(VENV)/bin/python
+
+# Python virtual environment version of the `pip` command
+VPIP := $(VENV)/bin/pip
 
 
 # =========================================================================== #
@@ -24,7 +51,15 @@
 
 # -- Python Targets -- #
 
-.PHONY: py-clean py-dist py-install py-release
+.PHONY: py-check py-clean py-dist py-install py-release py-test
+
+# Checks to see if Python is available from the PATH.
+py-check:
+	@if [ -z $(PYTHON) ]; then \
+		echo "'python3' could not be found."; \
+		echo "See https://docs.python.org/3/."; \
+		exit 2; \
+	fi
 
 # Removes all Python artifacts.
 py-clean: py-clean-build py-clean-pyc py-clean-test
@@ -43,9 +78,17 @@ py-install: py-clean
 py-release: py-dist
 	@twine upload dist/*
 
+# Sets up a Python package installation via pip.
+py-setup: requirements.txt
+	@pip install -r requirements.txt
+
+# Tests a Python package in a Python virtual environment.
+py-test: $(VACTIVATE)
+	@$(VPYTHON) -m unittest discover tests
+
 # -- Prerequisites for "py-clean" Target -- #
 
-.PHONY: py-clean-build py-clean-pyc py-clean-test
+.PHONY: py-clean-build py-clean-pyc py-clean-test py-clean-venv
 
 # Removes Python build artifacts.
 py-clean-build:
@@ -69,3 +112,21 @@ py-clean-test:
 	@rm -f .coverage
 	@rm -fr htmlcov/
 	@rm -fr .pytest_cache
+
+# Removes Python virtual environment artifacts.
+py-clean-venv:
+	@rm -fr $(VENV)
+
+
+# =========================================================================== #
+# NON-PHONY TARGETS
+# =========================================================================== #
+
+
+$(VACTIVATE): $(VPIP) requirements.txt
+	@$(VPIP) install -r requirements.txt
+
+$(VPIP): $(VPYTHON)
+
+$(VPYTHON): py-check
+	$(PYTHON) -m venv $(VENV)
